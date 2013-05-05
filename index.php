7a5267c354e5e1a213cfdc16fded2aa3999bf024
Christopher Lamm <chris@theantichris.com>
2013-05-05 15:22:57 -0400

    Initial files.

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: christopher
+ * Date: 5/5/13
+ * Time: 3:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+	<meta charset="utf-8" />
+
+	<title>iCalendar Creator</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
