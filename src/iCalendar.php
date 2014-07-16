5170d117f3074a37ee799a40c18992af549be52f
Christopher Lamm <chris@theantichris.com>
2014-07-16 11:26:15 -0400

    Setting properties.

--- src/iCalendar.php
+++ src/iCalendar.php
@@ -8,15 +8,26 @@ class iCalendar
     private $eventDescription;
     private $organizerName;
     private $organizerEmail;
-    private $timeZoneString;
-    private $timeZoneObject;
     private $utcOffset;
     private $eventStart;
     private $eventEnd;
     private $eventLocation;
 
-    function __construct($eventName)
+    function __construct($eventName, $eventDescription = '', $eventStart = '', $eventEnd = '', $eventLocation = '', $organizerName = '', $organizerEmail = '', $timeZoneString = '')
     {
-        $this->eventName = $eventName;
+        $this->eventName        = $eventName;
+        $this->eventDescription = $eventDescription;
+        $this->eventStart       = $eventStart;
+        $this->eventEnd         = $eventEnd;
+        $this->eventLocation    = $eventLocation;
+        $this->organizerName    = $organizerName;
+        $this->organizerEmail   = $organizerEmail;
+        $this->utcOffset        = $this->setUtcOffset($timeZoneString);
+    }
+
+    private function setUtcOffset($timeZoneString)
+    {
+        $timeZoneObject = new \DateTimeZone($timeZoneString);
+        return ($timeZoneObject->getOffset(new \DateTime())) * -1;
     }
 }
