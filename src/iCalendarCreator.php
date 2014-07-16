772fa92908bc41ea93e8d1d81cd05ad4a7b4d6c0
Christopher Lamm <chris@theantichris.com>
2014-07-16 14:52:03 -0400

    Added viewIcsFile().

--- src/iCalendarCreator.php
+++ src/iCalendarCreator.php
@@ -9,6 +9,13 @@ namespace theantichris\iCalendarCreator;
  */
 class iCalendarCreator
 {
+    /**
+     * Generates an ICS file.
+     *
+     * @since 1.0.0
+     *
+     * @param iCalendar $iCalendar
+     */
     public static function createIcsFile(iCalendar $iCalendar)
     {
         /** @var string $fileName */
@@ -17,6 +24,32 @@ class iCalendarCreator
         header("Content-Type: text/Calendar; charset=utf-8");
         header("Content-Disposition: inline; filename={$fileName}");
 
+        echo "BEGIN:VCALENDAR\n";
+        echo "VERSION:2.0\n";
+        echo "PRODID:-//{$iCalendar->getOrganizerName()}//NONSGML {$iCalendar->getEventName()}//EN\n";
+        echo "METHOD:REQUEST\n";
+        echo "BEGIN:VEVENT\n";
+        echo "UID:" . date('Ymd') . 'T' . date('His') . "-" . rand() . "-{$iCalendar->getOrganizerName()}\n";
+        echo "DTSTAMP:" . date('Ymd') . 'T' . date('His') . "\n";
+        echo "ORGANIZER:CN={$iCalendar->getOrganizerName()}:MAILTO:{$iCalendar->getOrganizerEmail()}\n";
+        echo "DTSTART:{$iCalendar->getEventStart()}\n";
+        echo "DTEND:{$iCalendar->getEventEnd()}\n";
+        echo "LOCATION:{$iCalendar->getEventLocation()}\n";
+        echo "SUMMARY:{$iCalendar->getEventName()}\n";
+        echo "DESCRIPTION: {$iCalendar->getEventDescription()}\n";
+        echo "END:VEVENT\n";
+        echo "END:VCALENDAR\n";
+    }
+
+    /**
+     * Echoes the ICS file information.
+     *
+     * @since 1.0.0
+     *
+     * @param iCalendar $iCalendar
+     */
+    public static function viewIcsFile(iCalendar $iCalendar)
+    {
         echo "BEGIN:VCALENDAR\n";
         echo "VERSION:2.0\n";
         echo "PRODID:-//{$iCalendar->getOrganizerName()}//NONSGML {$iCalendar->getEventName()}//EN\n";
