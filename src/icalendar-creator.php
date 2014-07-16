0f137ff55814f6c37e2dda77ee4a1a80f1e4247c
Christopher Lamm <chris@theantichris.com>
2014-07-16 08:58:30 -0400

    Renamed variables and methods to match PSR.

--- src/icalendar-creator.php
+++ src/icalendar-creator.php
@@ -2,49 +2,39 @@
 
 namespace theantichris\iCalendarCreator;
 
-require_once('icalendar-file.php'); // Require the class file.
+$eventName = 'Test Event';
 
-/* Setup data. */
+$eventDescription = 'This is the eventDescription for Test Event.';
 
-/*
- * Pass any parameters in the URL and use @$_GET[ 'parameter' ] to access them.
- */
+$organizer      = 'Christopher Lamm';
+$organizerEmail = 'chris@theantichris.com';
 
-$event_name = 'Test Event';
+$eventTimeZone = 'America/Chicago';
+date_default_timezone_set($eventTimeZone);
 
-$event_description = 'This is the description for Test Event.';
+$eventStart = mktime(8, 0, 0);
+$eventEnd   = mktime(18, 0, 0);
 
-$organizer       = 'Christopher Lamm';
-$organizer_email = 'chris@theantichris.com';
-
-$event_time_zone = 'America/Chicago';
-date_default_timezone_set($event_time_zone);
-
-$event_start = mktime(8, 0, 0);
-$event_end   = mktime(18, 0, 0);
-
-$venue = array(
-    'venue_name'        => 'Test Venue',
-    'venue_address'     => '2138 Wilson Rd.',
-    'venue_address_two' => 'Apt. B9',
-    'venue_city'        => 'Knoxville',
-    'venue_state'       => 'TN',
-    'venue_postal_code' => '37912'
+$location = array(
+    'locationName'       => 'Test Venue',
+    'locationAddress'    => '2138 Wilson Rd.',
+    'locationAddressTwo' => 'Apt. B9',
+    'locationCity'       => 'Knoxville',
+    'locationState'      => 'TN',
+    'locationPostalCode' => '37912'
 );
 
-/* Create iCalendar file. */
+$icalendar = new iCalendarFile($eventName);
 
-$icalendar = new iCalendarFile($event_name);
+$icalendar->setEventDescription($eventDescription);
+$icalendar->setOrganizer($organizer);
+$icalendar->setOrganizerEmail($organizerEmail);
+$icalendar->setEventStart($eventStart);
+$icalendar->setEventEnd($eventEnd);
+$icalendar->setTimeZone($eventTimeZone);
 
-$icalendar->set_event_description($event_description);
-$icalendar->set_organizer($organizer);
-$icalendar->set_organizer_email($organizer_email);
-$icalendar->set_event_start($event_start);
-$icalendar->set_event_end($event_end);
-$icalendar->set_time_zone($event_time_zone);
+$icalendar->setEventLocation($location);
 
-$icalendar->set_venue($venue);
+// $icalendar->createIcsFile(); // Creates the iCalendar file.
 
-// $icalendar->create_ics_file(); // Creates the iCalendar file.
-
-$icalendar->html_ics_file(); // Outputs the contents of the iCalendar file to HTML.
+$icalendar->htmlIcsFile(); // Outputs the contents of the iCalendar file to HTML.
--- src/icalendar-file.php
+++ src/icalendar-file.php
@@ -5,157 +5,157 @@ namespace theantichris\iCalendarCreator;
 class iCalendarFile
 {
     /** @var null|string The name for the event. */
-    public $event_name;
+    public $eventName;
     /** @var string The iCalendar file name. */
-    public $file_name;
-    /** @var null|string The event description. */
-    public $event_description;
+    public $fileName;
+    /** @var null|string The event eventDescription. */
+    public $eventDescription;
     /** @var null|string The organizer of the event. */
     public $organizer;
     /** @var null|string The email of the event organizer. */
-    public $organizer_email;
+    public $organizerEmail;
     /** @var null|string The event's time zone. */
-    public $time_zone;
-    /** @var object Time zone object created from $this->time_zone. */
-    public $time_zone_object;
+    public $timeZone;
+    /** @var object Time zone object created from $this->timeZone. */
+    public $timeZoneObject;
     /** @var integer Number of seconds between the event's time zone and UTC. */
-    public $utc_offset;
+    public $utcOffset;
     /** @var null|integer Event start time as an Epoch time stamp. */
-    public $event_start;
+    public $eventStart;
     /** @var null|integer Event end time as an Epoch time stamp. */
-    public $event_end;
+    public $eventEnd;
     /** @var array Venue information for the event. */
-    public $venue = array(
-        'venue_name',
-        'venue_address',
-        'venue_address_two',
-        'venue_city',
-        'venue_state',
-        'venue_postal_code'
+    public $eventLocation = array(
+        'locationName',
+        'locationAddress',
+        'locationAddress2',
+        'locationCity',
+        'locationState',
+        'locationPostalCode'
     );
 
-    public function __construct($event_name = null)
+    public function __construct($eventName = null)
     {
-        if (empty($event_name)) {
+        if (empty($eventName)) {
             die();
         } else {
-            $this->event_name = $event_name;
-            $this->file_name  = strtolower(str_replace(array(' ', "'", '.'), array('_', '', ''), $event_name)) . '.ics';
+            $this->eventName = $eventName;
+            $this->fileName  = strtolower(str_replace(array(' ', "'", '.'), array('_', '', ''), $this->eventName)) . '.ics';
         }
     }
 
-    public function set_event_description($event_description = null)
+    public function setEventDescription($eventDescription = null)
     {
-        if (!empty($event_description)) {
-            $this->event_description = $event_description;
+        if (!empty($eventDescription)) {
+            $this->eventDescription = $eventDescription;
         }
     }
 
-    public function set_organizer($organizer = null)
+    public function setOrganizer($organizer = null)
     {
         if (!empty($organizer)) {
             $this->organizer = $organizer;
         }
     }
 
-    public function set_organizer_email($organizer_email = null)
+    public function setOrganizerEmail($organizerEmail = null)
     {
-        if (!empty($organizer_email)) {
-            $this->organizer_email = $organizer_email;
+        if (!empty($organizerEmail)) {
+            $this->organizerEmail = $organizerEmail;
         }
     }
 
-    public function set_time_zone($time_zone = null)
+    public function setTimeZone($timeZone = null)
     {
-        if (!empty($time_zone)) {
-            $this->time_zone        = $time_zone;
-            $this->time_zone_object = new DateTimeZone($time_zone);
-            $this->utc_offset       = ($this->time_zone_object->getOffset(new DateTime())) * -1;
+        if (!empty($timeZone)) {
+            $this->timeZone       = $timeZone;
+            $this->timeZoneObject = new \DateTimeZone($timeZone);
+            $this->utcOffset      = ($this->timeZoneObject->getOffset(new \DateTime())) * -1;
         }
     }
 
-    public function set_event_start($event_start = null)
+    public function setEventStart($eventStart = null)
     {
-        if (!empty($event_start)) {
-            $this->event_start = $event_start;
+        if (!empty($eventStart)) {
+            $this->eventStart = $eventStart;
         }
     }
 
-    public function set_event_end($event_end = null)
+    public function setEventEnd($eventEnd = null)
     {
-        if (!empty($event_end)) {
-            $this->event_end = $event_end;
+        if (!empty($eventEnd)) {
+            $this->eventEnd = $eventEnd;
         }
     }
 
-    public function set_venue($venue = null)
+    public function setEventLocation($eventLocation = null)
     {
-        if ((!empty($venue)) || !is_array($venue)) {
-            $this->venue['venue_name']        = $venue['venue_name'];
-            $this->venue['venue_address']     = $venue['venue_address'];
-            $this->venue['venue_address_two'] = $venue['venue_address_two'];
-            $this->venue['venue_city']        = $venue['venue_city'];
-            $this->venue['venue_state']       = $venue['venue_state'];
-            $this->venue['venue_postal_code'] = $venue['venue_postal_code'];
+        if ((!empty($eventLocation)) || !is_array($eventLocation)) {
+            $this->eventLocation['locationName']       = $eventLocation['locationName'];
+            $this->eventLocation['locationAddress']    = $eventLocation['locationAddress'];
+            $this->eventLocation['locationAddress2']   = $eventLocation['locationAddress2'];
+            $this->eventLocation['locationCity']       = $eventLocation['locationCity'];
+            $this->eventLocation['locationState']      = $eventLocation['locationState'];
+            $this->eventLocation['locationPostalCode'] = $eventLocation['locationPostalCode'];
         }
     }
 
-    public function create_ics_file()
+    public function createIcsFile()
     {
         /** @var string $start Formatted start date and time. Converted to Zulu time. */
-        $start = date('Ymd', $this->event_start + $this->utc_offset) . 'T' . date('His', $this->event_start + $this->utc_offset) . 'Z';
+        $start = date('Ymd', $this->eventStart + $this->utcOffset) . 'T' . date('His', $this->eventStart + $this->utcOffset) . 'Z';
         /** @var string $end Formatted end date and time. Converted to Zulu time. */
-        $end = date('Ymd', $this->event_end + $this->utc_offset) . 'T' . date('His', $this->event_end + $this->utc_offset) . 'Z';
+        $end = date('Ymd', $this->eventEnd + $this->utcOffset) . 'T' . date('His', $this->eventEnd + $this->utcOffset) . 'Z';
 
         /** @var string $location Venue information combined into one string. */
-        $location = $this->venue['venue_name'] . ', ' . $this->venue['venue_address'] . ', ';
-        $location .= $this->venue['venue_address_two'] . ', ' . $this->venue['venue_city'] . ', ';
-        $location .= $this->venue['venue_state'] . ' ' . $this->venue['venue_postal_code'];
+        $location = $this->eventLocation['locationName'] . ', ' . $this->eventLocation['locationAddress'] . ', ';
+        $location .= $this->eventLocation['locationAddress2'] . ', ' . $this->eventLocation['locationCity'] . ', ';
+        $location .= $this->eventLocation['locationState'] . ' ' . $this->eventLocation['locationPostalCode'];
 
         header("Content-Type: text/Calendar; charset=utf-8");
-        header("Content-Disposition: inline; filename={$this->file_name}");
+        header("Content-Disposition: inline; filename={$this->fileName}");
         echo "BEGIN:VCALENDAR\n";
         echo "VERSION:2.0\n";
-        echo "PRODID:-//{$this->organizer}//NONSGML {$this->event_name}//EN\n";
+        echo "PRODID:-//{$this->organizer}//NONSGML {$this->eventName}//EN\n";
         echo "METHOD:REQUEST\n";
         echo "BEGIN:VEVENT\n";
         echo "UID:" . date('Ymd') . 'T' . date('His') . "-" . rand() . "-{$this->organizer}\n"; // Required by Outlook.
         echo "DTSTAMP:" . date('Ymd') . 'T' . date('His') . "\n";
-        echo "ORGANIZER:CN={$this->organizer}:MAILTO:{$this->organizer_email}\n";
+        echo "ORGANIZER:CN={$this->organizer}:MAILTO:{$this->organizerEmail}\n";
         echo "DTSTART:{$start}\n";
         echo "DTEND:{$end}\n";
         echo "LOCATION:{$location}\n";
-        echo "SUMMARY:{$this->event_name}\n";
-        echo "DESCRIPTION: {$this->event_description}\n";
+        echo "SUMMARY:{$this->eventName}\n";
+        echo "DESCRIPTION: {$this->eventDescription}\n";
         echo "END:VEVENT\n";
         echo "END:VCALENDAR\n";
     }
 
-    public function html_ics_file()
+    public function htmlIcsFile()
     {
         /** @var string $start Formatted start date and time. Converted to Zulu time. */
-        $start = date('Ymd', $this->event_start + $this->utc_offset) . 'T' . date('His', $this->event_start + $this->utc_offset) . 'Z';
+        $start = date('Ymd', $this->eventStart + $this->utcOffset) . 'T' . date('His', $this->eventStart + $this->utcOffset) . 'Z';
         /** @var string $end Formatted end date and time. Converted to Zulu time. */
-        $end = date('Ymd', $this->event_end + $this->utc_offset) . 'T' . date('His', $this->event_end + $this->utc_offset) . 'Z';
+        $end = date('Ymd', $this->eventEnd + $this->utcOffset) . 'T' . date('His', $this->eventEnd + $this->utcOffset) . 'Z';
 
         /** @var string $location Venue information combined into one string. */
-        $location = $this->venue['venue_name'] . ', ' . $this->venue['venue_address'] . ', ';
-        $location .= $this->venue['venue_address_two'] . ', ' . $this->venue['venue_city'] . ', ';
-        $location .= $this->venue['venue_state'] . ' ' . $this->venue['venue_postal_code'];
+        $location = $this->eventLocation['locationName'] . ', ' . $this->eventLocation['locationAddress'] . ', ';
+        $location .= $this->eventLocation['locationAddress2'] . ', ' . $this->eventLocation['locationCity'] . ', ';
+        $location .= $this->eventLocation['locationState'] . ' ' . $this->eventLocation['locationPostalCode'];
 
         echo "BEGIN:VCALENDAR<br />";
         echo "VERSION:2.0<br />";
-        echo "PRODID:-//{$this->organizer}//NONSGML {$this->event_name}//EN<br />";
+        echo "PRODID:-//{$this->organizer}//NONSGML {$this->eventName}//EN<br />";
         echo "METHOD:REQUEST<br />";
         echo "BEGIN:VEVENT<br />";
         echo "UID:" . date('Ymd') . 'T' . date('His') . "-" . rand() . "-{$this->organizer}<br />"; // Required by Outlook.
         echo "DTSTAMP:" . date('Ymd') . 'T' . date('His') . "<br />";
-        echo "ORGANIZER:CN={$this->organizer}:MAILTO:{$this->organizer_email}<br />";
+        echo "ORGANIZER:CN={$this->organizer}:MAILTO:{$this->organizerEmail}<br />";
         echo "DTSTART:{$start}<br />";
         echo "DTEND:{$end}<br />";
         echo "LOCATION:{$location}<br />";
-        echo "SUMMARY:{$this->event_name}<br />";
-        echo "DESCRIPTION: {$this->event_description}<br />";
+        echo "SUMMARY:{$this->eventName}<br />";
+        echo "DESCRIPTION: {$this->eventDescription}<br />";
         echo "END:VEVENT<br />";
         echo "END:VCALENDAR<br />";
     }
