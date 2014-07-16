93b498001a349009f0737c3d6918b4987bd9a857
Christopher Lamm <chris@theantichris.com>
2014-07-16 11:06:08 -0400

    Properties and constructor.

--- src/iCalendar.php
+++ src/iCalendar.php
@@ -4,5 +4,19 @@ namespace theantichris\iCalendarCreator;
 
 class iCalendar
 {
+    private $eventName;
+    private $eventDescription;
+    private $organizerName;
+    private $organizerEmail;
+    private $timeZoneString;
+    private $timeZoneObject;
+    private $utcOffset;
+    private $eventStart;
+    private $eventEnd;
+    private $eventLocation;
 
+    function __construct($eventName)
+    {
+        $this->eventName = $eventName;
+    }
 }
