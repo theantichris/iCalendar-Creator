9f66d1a4286de8dec0a0487484cc23c1f84eda6b
Christopher Lamm <chris@theantichris.com>
2014-07-16 15:09:31 -0400

    Added HTML line breaks to viewIcsFile().

--- src/iCalendarCreator.php
+++ src/iCalendarCreator.php
@@ -50,20 +50,20 @@ class iCalendarCreator
      */
     public static function viewIcsFile(iCalendar $iCalendar)
     {
-        echo "BEGIN:VCALENDAR\n";
-        echo "VERSION:2.0\n";
-        echo "PRODID:-//{$iCalendar->getOrganizerName()}//NONSGML {$iCalendar->getEventName()}//EN\n";
-        echo "METHOD:REQUEST\n";
-        echo "BEGIN:VEVENT\n";
-        echo "UID:" . date('Ymd') . 'T' . date('His') . "-" . rand() . "-{$iCalendar->getOrganizerName()}\n";
-        echo "DTSTAMP:" . date('Ymd') . 'T' . date('His') . "\n";
-        echo "ORGANIZER:CN={$iCalendar->getOrganizerName()}:MAILTO:{$iCalendar->getOrganizerEmail()}\n";
-        echo "DTSTART:{$iCalendar->getEventStart()}\n";
-        echo "DTEND:{$iCalendar->getEventEnd()}\n";
-        echo "LOCATION:{$iCalendar->getEventLocation()}\n";
-        echo "SUMMARY:{$iCalendar->getEventName()}\n";
-        echo "DESCRIPTION: {$iCalendar->getEventDescription()}\n";
-        echo "END:VEVENT\n";
-        echo "END:VCALENDAR\n";
+        echo "BEGIN:VCALENDAR<br />";
+        echo "VERSION:2.0<br />";
+        echo "PRODID:-//{$iCalendar->getOrganizerName()}//NONSGML {$iCalendar->getEventName()}//EN<br />";
+        echo "METHOD:REQUEST<br />";
+        echo "BEGIN:VEVENT<br />";
+        echo "UID:" . date('Ymd') . 'T' . date('His') . "-" . rand() . "-{$iCalendar->getOrganizerName()}<br />";
+        echo "DTSTAMP:" . date('Ymd') . 'T' . date('His') . "<br />";
+        echo "ORGANIZER:CN={$iCalendar->getOrganizerName()}:MAILTO:{$iCalendar->getOrganizerEmail()}<br />";
+        echo "DTSTART:{$iCalendar->getEventStart()}<br />";
+        echo "DTEND:{$iCalendar->getEventEnd()}<br />";
+        echo "LOCATION:{$iCalendar->getEventLocation()}<br />";
+        echo "SUMMARY:{$iCalendar->getEventName()}<br />";
+        echo "DESCRIPTION: {$iCalendar->getEventDescription()}<br />";
+        echo "END:VEVENT<br />";
+        echo "END:VCALENDAR<br />";
     }
 }
